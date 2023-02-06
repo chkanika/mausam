4a5dfcb356f518753c352217ad8ea90b700d5ba5
Kanika Chouhan <kanikachouhan@kanikas-mbp.ds.usfca.edu>
2023-02-05 17:36:30 -0800

    add

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/temperature")
 def temperature():
     return {
         "Temperature change"
         }
-    
-@app.route("/")
-def home():
-    return render_template("index.html")
+
+@app.route("/map")
+def map():
+    return render_template("index.html")    
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 4001, debug= True)
--- static/main.js
+++ static/main.js
@@ -1,3 +1,17 @@
+var inc = 0.1;
+var incStart = 0.005;
+var magInc = 0.0005;
+var start = 0;
+var scl = 10;
+var cols, rows;
+var zoff = 0;
+var fps;
+var particles = [];
+var numParticles = 500;
+var flowfield;
+var flowcolorfield;
+var magOff = 0;
+var showField = false;
 let angle = 0;
 let offset = 0;
 let bgColor = 0;
@@ -8,6 +22,11 @@ let temperature = 0;
 let minTemp = 0;
 let maxTemp = 300;
 let country = '';
+let xoff = 0;
+let yoff = 0;
+let xIncrement = 0.01;
+let yIncrement = 0.01;
+
 
 
 function mapTemperatureToColor(temp) {
@@ -19,125 +38,162 @@ function mapTemperatureToColor(temp) {
   
     return color(r, g, b);
   }
-
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  angle = radians(30);
-  branchRatio = 0.67;
-
  
+
+
   fetch('http://api.weatherapi.com/v1/current.json?key=2aa2387eb5c648b5b6b232109232801&q=[India]&aqi=no')
     .then(response => response.json())
     .then(data => {
       temperature = data.main.temp;
+
+      let temperature;
+
+    async function setup() {    
+  temperature = await getWeather();
+}
     });
 
     fill(mapTemperatureToColor(temperature));
 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  background(0);
+
+  for (let i = 0; i < numParticles; i++) {
+    particles[i] = new Particle();
+  }
+
+  flowfield = new Array(rows * cols);
+  flowcolorfield = new Array(rows * cols);
+}
+
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxSpeed = 2;
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
+  this.show = function(colorfield) {
+    strokeWeight(1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+    //point(this.pos.x, this.pos.y);
+  }
+
+  this.inverseConstrain = function(pos, key, f, t) {
+    if (pos[key] < f) {
+      pos[key] = t;
+      this.updatePrev();
+    }
+    if (pos[key] > t) {
+      pos[key] = f;
+      this.updatePrev();
+    }
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+
+  this.edges = function() {
+    this.inverseConstrain(this.pos, 'x', 0, width);
+    this.inverseConstrain(this.pos, 'y', 0, height);
+  }
+
+  this.follow = function(vectors, colorfield) {
+    let x = floor(this.pos.x / scl);
+    let y = floor(this.pos.y / scl);
+    let index = x + y * cols;
+    let force = vectors[index];
+    this.applyForce(force);
+    let c = colorfield[index];
+    if (c) {
+      stroke(color(c[0], c[1], c[2]));
+    }
+  }
 }
 
 function draw() {
-  background(bgColor);
-  translate(width / 2, height / 2);
-  stroke(255);
-
-  
-  fill(mapTemperatureToColor(temperature));
-  if (temperature >= 300) {
-    for (let i = 0; i < 4; i++) {
-      push();
-      let x = 100 * sin(time + i * 0.3) * sin(time);
-      let y = 100 * cos(time + i * 0.3) * cos(time);
-      translate(x, y);
-      rotate(i * TWO_PI / 4);
-      drawBranch(100 * sin(time * 0.3) + temperature / 100);
-      pop();
-    }
-  } else if (temperature >= 280) {
-    for (let i = 0; i < 4; i++) {
-      push();
-      let x = 100 * sin(time + i * 0.2) * sin(time);
-      let y = 100 * cos(time + i * 0.2) * cos(time);
-      translate(x, y);
-      rotate(i * TWO_PI / 4);
-      drawBranch(100 * sin(time * 0.2) + temperature / 100);
-      pop();
-    }
+  if (showField) {
+    background(0);
   } else {
-    for (let i = 0; i < 4; i++) {
-      push();
-      let x = 100 * sin(time + i * 0.1) * sin(time);
-      let y = 100 * cos(time + i * 0.1) * cos(time);
-      translate(x, y);
-      rotate(i * TWO_PI / 4);
-      drawBranch(100 * sin(time * 0.1) + temperature / 100);
-      pop();
+    background(color(0, 0, 0, 5));
+  }
+  var yoff = start;
+  for (let y = 0; y < rows; y++) {
+    let xoff = start;
+    for (let x = 0; x < cols; x++) {
+      let index = x + y * cols;
+      let r = noise(xoff, yoff, zoff) * 255;
+      let g = noise(xoff + 100, yoff + 100, zoff) * 255;
+      let b = noise(xoff + 200, yoff + 200, zoff) * 255;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle); // vector from angle
+      let m = map(noise(xoff, yoff, magOff), 0, 1, -5, 5);
+      v.setMag(m);
+      if (showField) {
+        push();
+        stroke(255);
+        translate(x * scl, y * scl);
+        rotate(v.heading());
+        let endpoint = abs(m) * scl;
+        line(0, 0, endpoint, 0);
+        if (m < 0) {
+          stroke('red');
+        } else {
+          stroke('green');
+        }
+        line(endpoint - 2, 0, endpoint, 0);
+        pop();
+      }
+      flowfield[index] = v;
+      flowcolorfield[index] = [r, g, b];
+      xoff += inc;
+    }
+    yoff += inc;
+  }
+  magOff += magInc;
+  zoff += incStart;
+  start -= magInc;
+
+  if (!showField) {
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].follow(flowfield, flowcolorfield);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show();
+    }
+
+    if (random(10) > 5 && particles.length < 2500) {
+      let rnd = floor(noise(zoff) * 20);
+      for (let i = 0; i < rnd; i++) {
+        particles.push(new Particle());
+      }
+    } else if (particles.length > 2000) {
+      let rnd = floor(random(10));
+      for (let i = 0; i < rnd; i++) {
+        particles.shift();
+      }
     }
   }
-
-
-  time += 0.05;
+  
 }
-
-function drawBranch(len) {
-  line(0, 0, 0, -len);
-  translate(0, -len);
-  if (len > 4) {
-    push();
-    rotate(angle * sin(time * 0.1) + temperature / 100);
-    drawBranch(len * branchRatio);
-    pop();
-    push();
-    rotate(-angle * sin(time * 0.1) - temperature / 100);
-    drawBranch(len * branchRatio);
-    pop();
-  }
-
- resetMatrix()
-
- function drawFern(x, y, size) {
-    // Draw the fern fractal using recursion
-    if (size > 2) {
-      push();
-      translate(x, y);
-      scale(size / 100);
-  
-      // Draw the main stem of the fern
-      stroke(0);
-      line(0, 0, 0, -size);
-      translate(0, -size);
-  
-      // Draw the first branch of the fern
-      drawFern(-size / 2, -size / 2, size / 2);
-  
-      // Draw the second branch of the fern
-      drawFern(size / 2, -size / 2, size / 2);
-  
-      // Draw the fronds of the fern
-      fill(mapTemperatureToColor(temperature));
-      noStroke();
-      ellipse(0, 0, size / 2, size / 2);
-      pop();
-    }
-  }
-
-  translate(width / 2, height / 2);
-  rotate(angle + mouseX/200);
-  translate(offset + mouseY/200, 0);
-
-  angle += 0.01;
-  offset += 0.1;
- 
-
-  setInterval(() => {
-    bgColor = color(random(255), random(255), random(255));
-  }, 3000 * 1000);
-}
-
-
-
-
-
-
-
